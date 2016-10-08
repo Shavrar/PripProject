d5479ace3de9f47b2b9fd3a0360db5620f0e62bc
Turlei Roman <r.turlei.rt@gmail.com>
2016-10-08 14:09:07 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        float factorial(int n)
+        {
+            float i, x = 1;
+            for (i = 1; i <= n; i++)
+            {
+                x *= i;
+            }
+            return x;
+        }
+
+        Dictionary<int, DayOfWeek> getMonth()
+        {
+            int days = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+
+            Dictionary<int, DayOfWeek> month = new Dictionary<int, DayOfWeek>();
+
+            month.Add(DateTime.Now.Day, DateTime.Now.DayOfWeek);
+
+            for (int i = DateTime.Now.Day - 1; i > 0; i--)
+            {
+                switch (month[i + 1])
+                {
+
+                    case DayOfWeek.Monday:
+
+                        month.Add(i, DayOfWeek.Sunday);
+                        break;
+
+                    case DayOfWeek.Tuesday:
+
+                        month.Add(i, DayOfWeek.Monday);
+                        break;
+
+                    case DayOfWeek.Wednesday:
+
+                        month.Add(i, DayOfWeek.Tuesday);
+                        break;
+
+
+                    case DayOfWeek.Thursday:
+
+                        month.Add(i, DayOfWeek.Wednesday);
+                        break;
+
+
+                    case DayOfWeek.Friday:
+
+                        month.Add(i, DayOfWeek.Thursday);
+                        break;
+
+
+                    case DayOfWeek.Saturday:
+
+                        month.Add(i, DayOfWeek.Friday);
+                        break;
+
+                    case DayOfWeek.Sunday:
+
+                        month.Add(i, DayOfWeek.Saturday);
+                        break;
+
+                }
+
+            }
+
+            for (int i = DateTime.Now.Day + 1; i <= days; i++)
+            {
+                switch (month[i - 1])
+                {
+
+                    case DayOfWeek.Monday:
+
+                        month.Add(i, DayOfWeek.Tuesday);
+                        break;
+
+                    case DayOfWeek.Tuesday:
+
+                        month.Add(i, DayOfWeek.Wednesday);
+                        break;
+
+                    case DayOfWeek.Wednesday:
+
+                        month.Add(i, DayOfWeek.Thursday);
+                        break;
+
+
+                    case DayOfWeek.Thursday:
+
+                        month.Add(i, DayOfWeek.Friday);
+                        break;
+
+
+                    case DayOfWeek.Friday:
+
+                        month.Add(i, DayOfWeek.Saturday);
+                        break;
+
+
+                    case DayOfWeek.Saturday:
+
+                        month.Add(i, DayOfWeek.Sunday);
+                        break;
+
+                    case DayOfWeek.Sunday:
+
+                        month.Add(i, DayOfWeek.Monday);
+                        break;
+
+                }
+
+            }
+
+            return month;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hello()
+        {
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Calendar()
+        {
+            int days = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+
+            Dictionary<int, DayOfWeek> month = getMonth();
+
+            Dictionary<int, String> newmonth = new Dictionary<int, String>();
+
+            int now = 0;
+
+            switch(month[1]){
+                case DayOfWeek.Monday:
+
+                    now = 0;
+                    break;
+
+                case DayOfWeek.Tuesday:
+
+                    now = 1;
+                    break;
+
+                case DayOfWeek.Wednesday:
+
+                    now = 2;
+                    break;
+
+
+                case DayOfWeek.Thursday:
+
+                    now = 3;
+                    break;
+
+
+                case DayOfWeek.Friday:
+
+                    now = 4;
+                    break;
+
+
+                case DayOfWeek.Saturday:
+
+                    now = 5;
+                    break;
+
+                case DayOfWeek.Sunday:
+
+                    now = 6;
+                    break;
+            }
+
+            ViewBag.now = now;
+
+            for (int i = 1; i <= now; i++)
+            {
+                newmonth.Add(i , null);
+            }
+
+            for (int i = 1; i <= days; i++)
+            {
+                newmonth.Add(i + now, month[i].ToString());
+            }
+
+            
+
+            if(days + now == 28)
+            {
+
+               now = days + now;
+               days = 28;
+
+                    
+            }
+            else if(days + now <= 35)
+            {
+                now = days + now;
+                days = 35;
+            }
+            else if(days + now <= 42)
+            {
+                now = days + now;
+                days = 42;
+            }
+            else
+            {
+                now = days + now;
+                days = 49;
+            }
+
+            if (days != now)
+            {
+                for (int i = now + 1; i <= days; i++)
+                {
+                    newmonth.Add(i, null);
+                }
+            }
+
+
+            ViewBag.days = days;
+
+            ViewBag.month = newmonth;
+
+            return View();
+        }
+
+        public ActionResult Contact(int id = 5)
+        {
+            ViewBag.Message = "Pascal Triangle";
+
+            int[][] a = new int[id][];
+
+            for(int i = 0; i < id; i++)
+            {
+                a[i] = new int[i + 1];
+
+                for (int c = 0; c <= i; c++)
+                {
+                    
+                    a[i][c]=(int)(factorial(i) / (factorial(c) * factorial(i - c))); 
+                }
+                
+            }
+            ViewBag.storage = a;
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/helpers/CalendarHelper.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace WebApplication2.helpers
+{
+    public static class CalendarHelper
+    {
+        public static string NameOfMonth(int a)
+        {
+            switch (a)
+            {
+                case 1:
+                    return "January";
+
+                case 2:
+                    return "February";
+
+                case 3:
+                    return "March";
+
+                case 4:
+                    return "April";
+
+                case 5:
+                    return "May";
+
+                case 6:
+                    return "June";
+
+                case 7:
+                    return "July";
+
+                case 8:
+                    return "August";
+
+                case 9:
+                    return "September";
+
+                case 10:
+                    return "October";
+
+                case 11:
+                    return "November";
+
+                case 12:
+                    return "December";
+            }
+
+            return "";
+        }
+
+        public static MvcHtmlString CreateCalendar(this HtmlHelper html, int month, int year, string day, int[] offdays)
+        {
+
+            int days = DateTime.DaysInMonth(year, month);
+
+            string[] daysofweek = { "Monday","Tuesday", "Wednesday","Thursday", "Friday","Saturday", "Sunday" };
+
+            int daysbefore = 0;
+            int daysafter = 0;
+
+            switch (day)
+            {
+                case "Monday":
+
+                    daysbefore = 0;
+                    break;
+
+                case "Tuesday":
+
+                    daysbefore = 1;
+                    break;
+
+                case "Wednesday":
+
+                    daysbefore = 2;
+                    break;
+
+
+                case "Thursday":
+
+                    daysbefore = 3;
+                    break;
+
+
+                case "Friday":
+
+                    daysbefore = 4;
+                    break;
+
+
+                case "Saturday":
+
+                    daysbefore = 5;
+                    break;
+
+                case "Sunday":
+
+                    daysbefore = 6;
+                    break;
+
+                default:
+
+                TagBuilder p = new TagBuilder("p");
+                p.SetInnerText("Wrong day name");
+                return new MvcHtmlString(p.ToString());
+            }
+
+            days += daysbefore;
+
+            
+             if (days <= 35 && days>28)
+            {
+                daysafter =35-days;
+                days = 35;
+            }
+            else if (days <= 42)
+            {
+                daysafter = 42 - days;
+                days = 42;
+            }
+            else
+            {
+                daysafter = 49 - days;
+                days = 49;
+            }
+
+            TagBuilder table = new TagBuilder("table");
+            table.AddCssClass("table table-bordered");
+
+            TagBuilder tr = new TagBuilder("tr");
+
+            for (int i = 0; i < 7; i++)
+            {
+                var td = new TagBuilder("td");
+                td.SetInnerText(daysofweek[i]);
+                tr.InnerHtml += td.ToString();
+                td = null;
+            }
+
+            table.InnerHtml += tr;
+
+            tr = null;
+
+            for (int i = 0; i < days/7; i++)
+            {
+                tr = new TagBuilder("tr");
+
+                for (int j = 1 + 7 * i,k = 1; j <= 7 + 7 * i; j++,k++)
+                {
+                    var td = new TagBuilder("td");
+                    if (j > daysbefore && j <= days-daysafter)
+                    {                        
+                        td.SetInnerText((j-daysbefore).ToString());
+                        if (k == 6 || k == 7)
+                        {
+                            td.AddCssClass("special");
+                        }
+                        if (offdays.Contains(j - daysbefore))
+                        {
+                            td.AddCssClass("special");
+                        }
+                        tr.InnerHtml += td.ToString();                       
+                    }
+                    else
+                    {
+                        
+                        tr.InnerHtml += td.ToString();
+                    }
+
+                    td = null;
+                }
+                table.InnerHtml += tr;
+                tr = null;
+            }
+
+            return new MvcHtmlString(table.ToString());
+
+        }
+    }
+}
